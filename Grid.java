b313e81b1064ed0653b8835316675148aba09545
PeterSokolov1028 <2018278@apps.nsd.org>
2023-03-17 10:55:19 -0700

    FIles: Formatting

--- Grid.java
+++ Grid.java
@@ -5,41 +5,38 @@
 
 import java.util.Scanner;
 
-public class Grid{
+public class Grid {
   int size;
   char[][][] activeGrid;
 
-  //Constructor set to default
-  public Grid(int size, char[][][] activeGrid){
+  // Constructor set to default
+  public Grid(int size, char[][][] activeGrid) {
     this.size = size;
     this.activeGrid = activeGrid;
   }
 
-
-
-  
-  //draws grid 
-  public void drawGrid(){
-    for(int k = 0; k < size;k++){
-      for(int i = 0 ; i < size; i++){
-       System.out.println();
-       for(int j = 0; j < size; j++){
-         System.out.print("[" + activeGrid[k][i][j] + "]");
-         }
-       }
+  // draws grid
+  public void drawGrid() {
+    for (int k = 0; k < size; k++) {
+      for (int i = 0; i < size; i++) {
+        System.out.println();
+        for (int j = 0; j < size; j++) {
+          System.out.print("[" + activeGrid[k][i][j] + "]");
+        }
+      }
       System.out.println();
     }
-    }
-  //gives grid initial values
-  public void initializeGrid(){
-    for(int k = 0; k < size; k++){
-      for(int i = 0 ; i < size; i++){
-        for(int j = 0; j < size; j++){
+  }
+
+  // gives grid initial values
+  public void initializeGrid() {
+    for (int k = 0; k < size; k++) {
+      for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
           activeGrid[k][i][j] = '-';
-         }
-       }
+        }
       }
     }
+  }
 
-  
 }
--- StatTracker.java
+++ StatTracker.java
@@ -3,35 +3,37 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 
-public class StatTracker{
-  //Fields
-   Player activePlayer;
-   File stats = new File("PlayerStats.csv");
-   File copyStats = new File("PlayerStats.csv");
+public class StatTracker {
+  // Fields
+  Player activePlayer;
+  File stats = new File("PlayerStats.csv");
+  File copyStats = new File("PlayerStats.csv");
 
-  //Constuctor
-  public StatTracker(Player activePlayer) throws FileNotFoundException{
+  // Constuctor
+  public StatTracker(Player activePlayer) throws FileNotFoundException {
     this.activePlayer = activePlayer;
   }
 
-  //Methods
-  public boolean hasPlayed()throws FileNotFoundException{
+  // Methods
+  public boolean hasPlayed() throws FileNotFoundException {
     Scanner scn = new Scanner(stats);
-    while(scn.hasNextLine()){
+    while (scn.hasNextLine()) {
       String nextLine = scn.nextLine();
-      String [] temp = nextLine.split(",");
-      for(int i = 0; i < temp.length;i++){
-        if(temp[i].equals(activePlayer.getName())) return true;
+      String[] temp = nextLine.split(",");
+      for (int i = 0; i < temp.length; i++) {
+        if (temp[i].equals(activePlayer.getName()))
+          return true;
       }
     }
     return false;
   }
-  public void addToFile(String a)throws FileNotFoundException{
+
+  public void addToFile(String a) throws FileNotFoundException {
     try (FileWriter writer = new FileWriter(stats, true)) {
       writer.write(a);
     } catch (Exception e) {
       e.printStackTrace();
     }
-    
+
   }
 }
