89a73ba6a6e3496f22ffaa0c30c04acfeb4650b4
Manas Kumar <70679511+Manas1114880@users.noreply.github.com>
2023-03-23 17:07:41 +0000

    Made changes

--- TTT.java
+++ TTT.java
@@ -1,15 +1,26 @@
 import java.util.Arrays;
+import java.util.*;
 
 public class TTT {
     // Properties
     private static final int SIZE = 3;
     private char[][][] state;
+    private boolean turn;
+    private boolean gameOn;
+    private String num;
+    private char actiaveChar = 'X';
 
     // Class constructor
     public TTT() {
+        this.turn = true;
+        this.gameOn = true;
         this.state = new char[SIZE][SIZE][SIZE];
-        System.out.println("I made TTT: " + this);
+        while(){
+            
+        }
+        move();
         draw();
+        System.out.println("I made TTT: " + this);
     }
 
     // Methods
@@ -18,12 +29,22 @@ public class TTT {
         for(char[][] board: state){
             for(char[] rows: board){
                 for(char x: rows){
+                    x = (x != 0)? ' ' : x;
                     System.out.printf("[ %c ]", x);
                 }
             }
             System.out.println();
         }
         System.out.println();
+    }
+
+    public void move(){
+        Scanner makeMove = new Scanner(System.in);
+        System.out.println("Gimme x y z");
+        int x = makeMove.nextInt();
+        int y = makeMove.nextInt();
+        int z = makeMove.nextInt();
+        state[x][y][z] = actiaveChar;
     }
 
     @Override
