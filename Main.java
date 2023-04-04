b363c116f1d42057851ae5065a0d825ac2c30b04
Israel Emmanuel <sriemmanuel787@outlook.com>
2023-04-04 10:46:19 -0700

    Main: Indentation fix

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 class Main {
-  public static void main(String[] args) throws 
-    FileNotFoundException {
-    Player Dan = new Player("dAN");
-    Dan.hasPlayed();
-      
-    TTT theGame = new TTT();
-    System.out.println("Let's make Tic-Tac-Toe");
-  }
+	public static void main(String[] args) throws FileNotFoundException {
+		Player Dan = new Player("dAN");
+		Dan.hasPlayed();
+
+		TTT theGame = new TTT();
+		System.out.println("Let's make Tic-Tac-Toe");
+	}
 }
